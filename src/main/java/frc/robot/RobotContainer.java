32cc6427aafeabdc1a000c625aeb58959a4b30c6
WindingMotor <kid.subudhi@gmail.com>
2022-10-13 10:01:22 -0400

    Working auto code! *DavidGamer*

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,7 +1,6 @@
-// File imports
+
 package frc.robot;
 import java.util.List;
-
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -10,9 +9,9 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-//import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
-//import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
@@ -39,7 +38,13 @@ public class RobotContainer {
   private final Joystick leftJoystick = new Joystick(OIConstants.kLeftJoystick);
   private final Joystick rightJoystick = new Joystick(OIConstants.kRightJoystick);
 
-  public RobotContainer() {
+  // Create PID controllers for trajectory tracking
+  private final PIDController xController = new PIDController(AutoConstants.kPXController, 0, 0);
+  private final PIDController yController = new PIDController(AutoConstants.kPYController, 0, 0);
+  private final ProfiledPIDController thetaController = new ProfiledPIDController(AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+
+
+  public RobotContainer(){
 
     // Set swerve subsystem default command to swerve joystick with respective joystick inputs
     swerveSubsystem.setDefaultCommand(new SwerveJoystick(swerveSubsystem,
@@ -49,46 +54,27 @@ public class RobotContainer {
     () -> !leftJoystick.getRawButton(2 /* Place button value here! FIELD ORIENTED? */)));
 
     configureButtonBindings();
-    
   }
 
   // Create buttons bindings
-  private void configureButtonBindings() {
+  private void configureButtonBindings(){
 
     // Assign button to manually zero heading
     new JoystickButton(rightJoystick,2).whenPressed(() -> swerveSubsystem.zeroHeading());
-
   }
 
 
-  private Command testAuto = new TestAuto(swerveSubsystem);
+  // Create testAuto command
+  private Command testAuto = new TestAuto(swerveSubsystem, xController, yController, thetaController);
   
   public Command getAutonomousCommand() {
 
-    // Create trajectory settings
-    TrajectoryConfig trajectoryConfig = new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond, AutoConstants.kMaxAccelerationMetersPerSecondSquared).setKinematics(DriveConstants.kDriveKinematics);
+    Command autoCommand = null;
 
-    // Generate trajectory
-    Trajectory trajectory = TrajectoryGenerator.generateTrajectory(new Pose2d(0,0,new Rotation2d(0)), List.of(
-    // Interior points
-    new Translation2d(1,0),
-    new Translation2d(1,-1)), 
-    // Ending point
-    new Pose2d(2,-1, Rotation2d.fromDegrees(180)), trajectoryConfig);
+    autoCommand = testAuto;
 
-    // Create PID controllers for trajectory tracking
-    PIDController xController = new PIDController(AutoConstants.kPXController, 0, 0);
-    PIDController yController = new PIDController(AutoConstants.kPYController, 0, 0);
-    ProfiledPIDController thetaController = new ProfiledPIDController(AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+    return autoCommand;
 
-    // Make theta PID controller think its a circle
-    thetaController.enableContinuousInput(-Math.PI, Math.PI);
-
-    // Create command to follow trajectory
-    SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(trajectory, swerveSubsystem::getPose, DriveConstants.kDriveKinematics, xController, yController, thetaController, swerveSubsystem::setModuleStates, swerveSubsystem);
-
-    // Return auto command to run
-    return new SequentialCommandGroup(new InstantCommand(() -> swerveSubsystem.resetOdometry(trajectory.getInitialPose())), swerveControllerCommand, new InstantCommand(() -> swerveSubsystem.stopModules())); // Turn to point at center 
   }
 
 
--- src/main/java/frc/robot/auto/paths/Path.java
+++
@@ -1,7 +0,0 @@
-
-package frc.robot.auto.paths;
-import frc.robot.auto.util.SwerveTrajectory;
-
-public abstract class Path {
-    public abstract SwerveTrajectory getPath();
-}
