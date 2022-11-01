5ad27939b3e86bb02207f3791e529f14d7257318
WindingMotor <kid.subudhi@gmail.com>
2022-11-01 16:56:56 -0400

    encoder changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -58,19 +58,19 @@ public final class Constants {
         public static final boolean kBackRightDriveEncoderReversed = false;
 
         public static final int kFrontLeftDriveAbsoluteEncoderPort = 0;
-        public static final int kBackLeftDriveAbsoluteEncoderPort = 2;
+        public static final int kBackLeftDriveAbsoluteEncoderPort = 3;
         public static final int kFrontRightDriveAbsoluteEncoderPort = 1;
-        public static final int kBackRightDriveAbsoluteEncoderPort = 3;
+        public static final int kBackRightDriveAbsoluteEncoderPort = 2;
 
         public static final boolean kFrontLeftDriveAbsoluteEncoderReversed = false;
         public static final boolean kBackLeftDriveAbsoluteEncoderReversed = false;
         public static final boolean kFrontRightDriveAbsoluteEncoderReversed = false;
         public static final boolean kBackRightDriveAbsoluteEncoderReversed = false;
 
-        public static final double kFrontLeftDriveAbsoluteEncoderOffsetRad = 0;  //-0.254
-        public static final double kBackLeftDriveAbsoluteEncoderOffsetRad = 0;   // -1.252
-        public static final double kFrontRightDriveAbsoluteEncoderOffsetRad = 0; // -1.816
-        public static final double kBackRightDriveAbsoluteEncoderOffsetRad = 0;  // -4.811
+        public static final double kFrontLeftDriveAbsoluteEncoderOffsetRad = -0.254;
+        public static final double kBackLeftDriveAbsoluteEncoderOffsetRad = 1.252;
+        public static final double kFrontRightDriveAbsoluteEncoderOffsetRad = -1.816;
+        public static final double kBackRightDriveAbsoluteEncoderOffsetRad =  -4.811;
 
         public static final double kPhysicalMaxSpeedMetersPerSecond = 5;
         public static final double kPhysicalMaxAngularSpeedRadiansPerSecond = 2 * 2 * Math.PI;
